c7d464126e28b76ea78385af55ebc57a17968b81
Dimitri Emmerich <de@Dimitris-iMac.local>
2019-07-12 18:30:09 +0200

    init

---
+++ angular-8-regloghome/src/app/_models/user.ts
@@ -0,0 +1,8 @@
+﻿export class User {
+    id: number;
+    login:string;
+    name: string;
+    password: string;
+    email: string;
+    token: string;
+}
---
+++ angular-8-regloghome/src/app/register/register.component.ts
@@ -0,0 +1,65 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AlertService, UserService, AuthenticationService } from '@/_services';
+
+@Component({ templateUrl: 'register.component.html' })
+export class RegisterComponent implements OnInit {
+    registerForm: FormGroup;
+    loading = false;
+    submitted = false;
+
+    constructor(
+        formBuilder: FormBuilder,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private userService: UserService,
+        private alertService: AlertService
+    ) {
+        // redirect to home if already logged in
+        if (this.authenticationService.currentUserValue) {
+            this.router.navigate(['/']);
+        }
+
+        this.registerForm = formBuilder.group({
+            email: ['', Validators.required],
+            //name: ['', Validators.required],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+    ngOnInit() {
+
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.userService.register(this.registerForm.value)
+            .subscribe(
+                data => {
+                    console.log(data);
+                    this.alertService.success('Registration successful', true);
+                    this.router.navigate(['/login']);
+                },
+                error => {
+                    this.alertService.error(error.toString());
+                    this.loading = false;
+                });
+        //todo disposable hinzufügen
+    }
+}
